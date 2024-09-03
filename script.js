3c5b456c4127dbecf49208648050431847b06d78
camilayacumalvargas <mariacamilayacumal@iemmmrip.edu.co>
2024-09-03 11:26:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var ne=8;
+function adivinar(){
+    var nd=document.getElementById("barra").value;
+    if(ne==nd){
+        alert("GANASTE");
+    }else if(ne>nd){
+        alert("EL NÚMERO ES MAYOR");
+    }else{
+        alert("EL NÚMERO ES MENOR");
+    }
+}
